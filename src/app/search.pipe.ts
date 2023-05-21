5061a48fd875803ae85bc359c470c08de6bb00e5
Dan Cornilov <dancornilov@Dans-MacBook-Pro.local>
2023-05-22 01:50:27 +0300

    fix: remove uselss console log

--- src/app/search.pipe.ts
+++ src/app/search.pipe.ts
@@ -5,7 +5,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class SearchPipe implements PipeTransform {
   transform(value: string | null, options: any[] = [], property: string = 'name'): any[] {
-    console.log(value);
     return options.filter(item => (item[property].toLowerCase()).includes(value?.toLowerCase()));
   }
 }
